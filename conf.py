d193158023d48e7e6d35ed9895be32435b8d0553
Jonathan Sick <jsick@lsst.org>
2016-06-15 14:54:29 -0700

    Refactor release note source
    - Release notes for each release are contained in separate files that
      are 'included' in the main release-notes.rst file. The helps make the
      release notes files a manageable length
    - Convert template into a README.md to provide in situ author
      instructions

--- conf.py
+++ conf.py
@@ -88,7 +88,8 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build',
+                    'releases/note-source']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
