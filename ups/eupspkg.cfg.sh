4a16340149598745d9261a272ed78beeace8fe82
Kian-Tat Lim <ktl@slac.stanford.edu>
2021-04-22 09:33:12 -0700

    Compensate for SIP on macOS.

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,3 +1,8 @@
 build() {
+    # macOS SIP swallows DYLD_LIBRARY_PATH so export the duplicate in
+    # LSST_LIBRARY_PATH
+    if [[ -z $DYLD_LIBRARY_PATH ]]; then
+        export DYLD_LIBRARY_PATH=$LSST_LIBRARY_PATH
+    fi
     stack-docs build
 }
