6401d52841ddd82971460d647d2265b7bcd7eb5e
Matthias Wittgen <matthias.michael.wittgen@cern.ch>
2023-12-03 11:27:28 -0800

    Update conf.py for v26

--- conf.py
+++ conf.py
@@ -15,21 +15,21 @@ html_short_title = project
 # Patch EUPS tag substitutions
 rst_epilog = """
 
-.. |eups-tag| replace:: v25_0_2
-.. |eups-tag-mono| replace:: ``v25_0_2``
-.. |eups-tag-bold| replace:: **v25_0_2**
+.. |eups-tag| replace:: v26_0_0
+.. |eups-tag-mono| replace:: ``v26_0_0``
+.. |eups-tag-bold| replace:: **v26_0_0**
 """
 
 # Patch EUPS and Git tag context for Jinja templating
 jinja_contexts = {
     "default": {
-        "release_eups_tag": "v25_0_2",
-        "release_git_ref": "25.0.2",
-        "version": "v25_0_2",
-        "release": "v25_0_2",
-        "scipipe_conda_ref": "25.0.2",
-        "pipelines_demo_ref": "25.0.2",
-        "newinstall_ref": "25.0.2",
+        "release_eups_tag": "v26_0_0",
+        "release_git_ref": "26.0.0",
+        "version": "v26_0_0",
+        "release": "v26_0_0",
+        "scipipe_conda_ref": "26.0.0",
+        "pipelines_demo_ref": "26.0.0",
+        "newinstall_ref": "26.0.0",
     }
 }
 
